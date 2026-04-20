978adc35c6d0ff8d2f309c727202ced297c29eea
DESKTOP-L7D3DA6\siade <sia.derupen1118@gmail.com>
2026-04-20 15:08:32 +0900

    fix: Edge runtime用に環境変数をnext.configに明示

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    BASIC_AUTH_USER: process.env.BASIC_AUTH_USER ?? '',
+    BASIC_AUTH_PASS: process.env.BASIC_AUTH_PASS ?? '',
+  },
 };
 
 export default nextConfig;
